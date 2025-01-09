94a100a5e59b44c8db4fa5fdf4b3aad1eaab0efe
Time-11-emiT <harshdeshpande2010@gmail.com>
2025-01-10 00:03:01 +0530

    Added titanic data analysis

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # interpretability_with_captum
-This is a repository containing a few experiments and side-projects which I've worked on to explore interpretability with captum
+This is a repository containing a few experiments and side-projects which I've worked on to explore interpretability with captum.
---
+++ titanic_data_analysis/main.py
@@ -0,0 +1,12 @@
+# %%
+import numpy as np
+import torch
+from captum.attr import IntegratedGradients
+from captum.attr import LayerConductance
+from captum.attr import NeuronConductance
+import  matplotlib
+import matplotlib.pyplot as plt
+# %matplotlib inline
+from scipy import stats
+import pandas as pd
+# %%
